2d25e3356c848219da9fe7f4b36240f027f79995
Ty Schaub <tyschaub@Tys-MacBook-Air.local>
2025-09-18 14:44:51 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)
+    {
+        double springCost = 10000;
+        double summerCost = 8000;
+        double fallCost = 6000;
+        double winterCost = 12000;
+        double annualCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("The cost for Fall " + fallCost + " and the cost for Spring" + springCost + " and the cost for Winter" + winterCost + " and the cost for Summer" + summerCost + " is " + annualCost);
+    }
+}
